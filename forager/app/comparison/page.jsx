497cce31ea721835f641f38fd40766ac7225cd2b
Dong Nguyen <94870408+cowboy333@users.noreply.github.com>
2025-03-09 23:19:28 -0500

    update comparison

--- forager/app/comparison/page.jsx
+++ forager/app/comparison/page.jsx
@@ -2,8 +2,10 @@ import NavBar from '../../components/NavBar';
 
 export default function MushroomComparisonPage() {
   return (
-    <div className="page">
-      <h1>Mushroom Comparison Page</h1>
+    <div className="page bg-white">
+      <div class="flex flex-col items-center text-center bg-[rgb(87,144,118)] justify-start pt-20 mb-10 text-white">
+        <h3 class="text-4xl font-bold">Compare</h3>
+      </div>
       <NavBar />
     </div>
   );

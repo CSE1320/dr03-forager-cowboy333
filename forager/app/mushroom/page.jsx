2c38c4771b28d962fea6833e0e0d32667bb65f90
Dong Nguyen <94870408+cowboy333@users.noreply.github.com>
2025-03-09 23:20:35 -0500

    update mushroom

--- forager/app/mushroom/page.jsx
+++ forager/app/mushroom/page.jsx
@@ -3,11 +3,14 @@ import NavBar from '../../components/NavBar';
 
 export default function MushroomPage() {
   return (
-    <div className="page">
-      <h1>Mushroom Page</h1>
-      <Link href="/comparison">
-        <button>Go to Mushroom Comparison Page</button>
+    <div className="bg-white min-h-screen text-black">
+      <div class="flex flex-col items-center text-center bg-[rgb(87,144,118)] justify-start pt-20 mb-10 text-white">
+        <h3 class="text-4xl font-bold">Match Results</h3>
+      </div>
+      <Link href="/comparison" className="flex justify-end pr-10">
+        <button>Comparison</button>
       </Link>
+
       <NavBar />
     </div>
   );
